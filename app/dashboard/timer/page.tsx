c7bd2f68e5145cee7ebdb68df79988c695066dea
jeronimo2007 <jeronimogomez0109@gmail.com>
2025-02-12 06:05:42 -0500

    agregando tema oscuro v8

--- app/dashboard/timer/page.tsx
+++ app/dashboard/timer/page.tsx
@@ -12,7 +12,7 @@ import { Label } from "@/components/ui/label";
 import * as Dialog from "@radix-ui/react-dialog";
 import { DateClickArg } from '@fullcalendar/interaction';
 import { EventClickArg } from '@fullcalendar/core';
-import "@/styles/fullcalendar-theme.css";
+import "@/styles/fullcalendar-theme";
 
 interface Project {
   id: string;
